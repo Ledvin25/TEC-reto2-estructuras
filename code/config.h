f644d9ed3522db0078ea2d8f917f21abde75c9ff
Alejandro Gutiérrez <alejandrogutierrezchaves@gmail.com>
2023-09-30 00:03:31 -0600

    settings

--- code/config.h
+++ code/config.h
@@ -2,17 +2,19 @@
 
 // Aqui se van a trabajar todos las configuraciones del simulador como con sus correspondientes setters y getters
 
-struct Settings {
-    int simulationTime;      // 1. El tiempo de simulación
-    int arrivalTime;         // 2. El tiempo de llegada de los clientes
-    int orderWindow;         // 3. Ventanas para pedido
-    int orderSpeed;          // 4. Velocidad para atender pedido
-    int deliveryWindow;      // 5. Ventana para retirar pedido
-    int deliverySpeed;       // 6. Velocidad para retirar pedido
-    int maxPerWindow;        // 7. Cantidad máxima por ventana
+struct Setting {
+    int simulationTime;
+    int minHora;
+    int maxHora;
+    int velocidadDeLlegada;
+    int ventanasParaPedido;
+    int velocidadParaAtender;
+    int ventanasParaRetirar;
+    int velocidadParaRetirar;
+    int cantMaximaPorVentana;
+    int cantidadMaxIngrediente;
 
-    // Constructor para inicializar todas las configuraciones
-    Settings() : simulationTime(0), arrivalTime(0), orderWindow(0),
-                 orderSpeed(0), deliveryWindow(0), deliverySpeed(0),
-                 maxPerWindow(0) {}
+    Setting() : simulationTime(0), minHora(0), maxHora(0), velocidadDeLlegada(0),
+                 ventanasParaPedido(0), velocidadParaAtender(0), ventanasParaRetirar(0),
+                 velocidadParaRetirar(0), cantMaximaPorVentana(0), cantidadMaxIngrediente(0) {}
 };
